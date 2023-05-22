2d144ba399cbbc14e1cf514ad438ab835917530a
duchuykg <duchuydeptrainhatthegioi@gmail.com>
2023-05-22 23:57:29 +0700

    HuyPham: SigninScreen

--- app/screens/Login/Signin.js
+++ app/screens/Login/Signin.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { ImageBackground } from "react-native";
 import { Image, StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Alert} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Login = ({ navigation }) => {
   const handleLogin = () => {
@@ -23,78 +24,127 @@ const Login = ({ navigation }) => {
 
   return (
     <>
-        <View style={styles.content1}>
-          <Text style={styles.textlogin}>Login</Text>
+    <View style = {styles.container}>
 
-        
-
-          <View style={styles.form}>
-            <TextInput
-              style={styles.input}
-              placeholder="Tên đăng nhập"
-              onChangeText={(text) => setUsername(text)}
-            />
+      <View style = {styles.container1}>
+        <TouchableOpacity style={styles.thanh} onPress={handleLogin}>
+          <View style={styles.row}>
+            <Text style={styles.thanhText}>Welcome to BusPlus as a guest</Text>
+            <Ionicons style={styles.padd} name={"chevron-forward-outline"} size={20} color={"#FFFFFF"}></Ionicons>
           </View>
+        </TouchableOpacity>
+      </View>
 
-          <View style={styles.form1}>
+      <View style={styles.container2}>          
+        <Image style={styles.logo} source={require("../../assets/test.png")} />
+      </View>
+
+      <View style={styles.container2}>        
+        <View style={styles.form}>
           <TextInput
-              style={styles.input}
-              placeholder="Mật khẩu"
-              secureTextEntry={true}
-              onChangeText={(text) => setPassword(text)}
-            />
-            <TouchableOpacity onPress={handleForgot}>
-              <View>
-                <Text style={[styles.caption2 ]}>Forgot PassWord</Text>
-              </View>
-            </TouchableOpacity>    
-          </View>
+            style={styles.input}
+            placeholder="Username"
+            // onChangeText={(text) => setUsername(text)}
+          />
         </View>
+      </View>
 
-        <View style={styles.content1}>
-          <View style={styles.form2}>
-            
-            <TouchableOpacity style={styles.button} onPress={handleLogin}>
-              <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-          </View>    
-        </View>    
-
-        <View style={styles.content}>
-          <View style={styles.form3}>
-            <TouchableOpacity onPress={handleHelp} style={styles.helpcontact}>
-              <View>
-                <Text style={[styles.caption3 ]}>Help</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleContact} style={styles.helpcontact}>
-              <View>
-                <Text style={[styles.caption3 ]}>Contact</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleAboutUs} style={styles.helpcontact}>
-              <View>
-                <Text style={[styles.caption3 ]}>About Us</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
+      <View style={styles.container2}>     
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <TouchableOpacity onPress={handleForgot}>
+            <View>
+              <Text style={styles.caption}>Forgot PassWord</Text>
+            </View>
+          </TouchableOpacity>    
         </View>
+      </View>
+
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.thanhText}>Welcome to BusPlus</Text>
+        </TouchableOpacity>
+      </View>    
+
+      <View style = {styles.container3}>
+        <Image style={styles.logo1} source={require("../../assets/fb.png")} />
+        <Image style={styles.logocenter} source={require("../../assets/mail.png")} />
+        <Image style={styles.logo1} source={require("../../assets/dt.png")} />
+      </View>
+
+
+      <View style = {styles.container3}>
+        <TouchableOpacity style={styles.thanh} onPress={handleLogin}>
+          <View style={styles.row}>
+            <Text style={styles.thanhText}>Creat Account</Text>
+            <Ionicons style={styles.padd} name={"chevron-forward-outline"} size={20} color={"#FFFFFF"}></Ionicons>
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
-
-  container1: {
-    flex: 1,
-    justifyContent: "space-between",
-  },
-
   container: {
     flex: 1,
     width: "100%",
+    justifyContent: "space-between",
+    backgroundColor: 'white',
+  },
+  container1: {
+    flexDirection: "row",
+    paddingTop: 40,
     justifyContent: "center",
+  },
+  container3: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  thanh: {
+    width: "100%",
+    height: 55,
+    backgroundColor: "#C84771",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  thanhText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  padd: {
+    paddingStart: 12,
+  },
+  container2: {
+    alignItems: "center",
+    justifyContent:"center",
+  },
+  logo: {
+    width: "100%",
+    resizeMode: "contain",
+  },
+
+  logo1: {
+    resizeMode: "contain",
+  },
+
+  logocenter: {
+    resizeMode: "contain",
+    marginLeft: 40,
+    marginRight: 40,
   },
 
   content: {
@@ -104,80 +154,30 @@ const styles = StyleSheet.create({
 
   },
 
-  content1: {
-    justifyContent: "center", // can giua theo truc y
-    alignItems: "center", // can giua theo truc x
-    padding: 5,
-
-  },
-  formlogo: {
-    flexDirection: "row",
-    alignItems: "flex-end",
-    justifyContent:"center",
-    marginTop: 70,
-  
-  },
-
-  logo: {
-    width: 260,
-    height: 50,
-    resizeMode: "contain",
-    // position: "absolute",
-    // top: 60,
-    // marginBottom: 30,
-  },
-  form0: {
-    flexDirection: "row",
-    padding: 5,
-    alignItems: "center",
-    justifyContent: "space-between",
-    
-  },
-
   form: {
-    flexDirection: "row",
-    marginTop: 25,
     alignItems: "center",
     width: "100%",
-    maxWidth: 300,
-    borderRadius: 20,
-
   },
+
   form1: {
     width: "100%",
-    marginTop: 25,
-    maxWidth: 300,
-    borderRadius: 20,
-    justifyContent: "space-between",
   },
-  form2: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
-    padding: 5,
-    borderRadius: 20,
-  },
-
-  form3: {
-    flexDirection: 'row',
-    marginBottom: 30,
-  },
-
+  
   input: {
-    width: "100%",
-    maxWidth: 300,
+    width: "90%",
     height: 50,
-    backgroundColor: "#fff",
-    borderRadius: 20,
-    paddingHorizontal: 25,
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderColor: '#C7C7CC',
+    borderWidth: 1,
+    padding: 10,
   },
+  
   button: {
-    width: "100%",
-    height: 50,
-    backgroundColor: "#F8D247",
+    width: "80%",
+    height: 45,
+    backgroundColor: "#C84771",
     borderRadius: 30,
-    marginTop: 10,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -191,22 +191,11 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: "bold",
   },
-  caption2: {
-    color: 'white',
+  caption: {
+    color: 'black',
     fontWeight: "bold",
     textAlign: 'right',
   },
-
-  caption3: {
-    color: 'gray',
-    fontWeight: "bold",
-  },
-
-  helpcontact: {
-    marginLeft: 20, 
-
-  },
-
 
 });
 
