679a60d22008ab858ed9d98b79857659c179b6aa
duchuykg <duchuydeptrainhatthegioi@gmail.com>
2023-05-23 22:31:19 +0700

    duchuy: podcastscreen

--- app/screens/Podcast/PodCastScreen.js
+++ app/screens/Podcast/PodCastScreen.js
@@ -1,18 +1,45 @@
 import {
     SafeAreaView,
+    Image,
     View,
     Text,
     StyleSheet,
     TextInput,
     TouchableOpacity,
     StatusBar,
+    FlatList,
   } from "react-native";
   import React, { useState, useEffect } from "react";
-  import MapView from "react-native-maps";
   import Ionicons from "react-native-vector-icons/Ionicons";
   
   export default function PodCastScreen({ navigation }) {
-    const [text, onChangeText] = React.useState("Useless Text");
+    const [tags, setTags] = useState([
+      { id: "1", label: "All" },
+      { id: "2", label: "Life" },
+      { id: "3", label: "Tech" },
+      { id: "4", label: "Comedy" },
+    ]);
+    // const [selectedTagId, setSelectedTagId] = useState(null);
+
+    const renderTag = ({ item }) => (
+      <TouchableOpacity
+        style={[
+          styles.tagButton,
+          item.id === "1" && styles.tagButtonChoice,
+        ]}
+        // onPress={() => setSelectedTagId(item.id)}
+      >
+        <Text
+          style={[
+            styles.tagText,
+            item.id === "1" && styles.tagChoice,
+          ]}
+        >
+          {item.label}
+        </Text>
+      </TouchableOpacity>
+    );
+    
     return (
       <SafeAreaView style={styles.container}>
         
@@ -30,7 +57,55 @@ import {
               placeholder="Search the podcast here ..."
             />
           </View>
+
+          <View style={styles.top}>
+            <FlatList
+              data={tags}
+              horizontal={true}
+              renderItem={renderTag}
+              keyExtractor={(item) => item.id}
+              contentContainerStyle={styles.tagContainer}
+            />
+          </View>
+
+          <View style={styles.top}>
+            <Text style={styles.trend}>Trending</Text>
+          </View>
+
+          <View style={styles.podcast}>
+            <View style={styles.trendingContainer}>
+              <View style={styles.trendingColumn}>
+                <TouchableOpacity onPress={() => navigation.navigate("List")}>
+                  <Image style={styles.logo} source={require("../../assets/p1.png")} />
+                  <Text style={styles.name}>MÂY PodCast</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.trendingColumn}>
+                <TouchableOpacity onPress={() => navigation.navigate("List")}>
+                  <Image style={styles.logo} source={require("../../assets/p2.png")} />
+                  <Text style={styles.name}>MÂY PodCast</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.trendingContainer}>
+              <View style={styles.trendingColumn}>
+                <TouchableOpacity onPress={() => navigation.navigate("List")}>
+                  <Image style={styles.logo} source={require("../../assets/p1.png")} />
+                  <Text style={styles.name}>MÂY PodCast</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.trendingColumn}>
+                <TouchableOpacity onPress={() => navigation.navigate("List")}>
+                  <Image style={styles.logo} source={require("../../assets/p2.png")} />
+                  <Text style={styles.name}>MÂY PodCast</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            
+          </View>
         </View>
+
+        
   
       </SafeAreaView>
     );
@@ -41,32 +116,6 @@ import {
       flex: 1,
       backgroundColor: "white",
     },
-  
-    lookup_text: {
-      alignItems: "center",
-    },
-  
-    lookup_text_header: {
-      fontSize: 20,
-      fontWeight: 500,
-    },
-  
-    lookup_text_body: {
-      fontSize: 14,
-      color: "#C7C5CD",
-    },
-    lookup: {
-      position: "absolute",
-      alignItems: "center",
-      bottom: 16,
-      left: 16,
-      right: 16,
-      width: "45%",
-      height: 122,
-      backgroundColor: "white",
-      borderRadius: 20,
-    },
-  
     searchSection: {
       position: "absolute",
       top: 16 + StatusBar.currentHeight,
@@ -85,25 +134,76 @@ import {
       width: "100%",
       borderColor: "#F5C0D2",
       backgroundColor: "white",
+      marginTop: 10,
     },
     searchIcon: {
       padding: 10,
-    },
-  
-    searchIcon_lookup: {
-      paddingBottom: 5,
-      paddingTop: 13,
     },
   
     input: {
       flex: 1,
     },
     welcome: {
-        fontWeight: "bold",
-        fontSize: 18,
-        width: '100%',
-        textAlign: "center",
-        paddingBottom: 15,
-      },
+      fontWeight: "bold",
+      fontSize: 18,
+      width: '100%',
+      textAlign: "center",
+      paddingBottom: 15,
+    },
+    tagContainer: {
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 10,
+    },
+    tagButton: {
+      backgroundColor: "#F4F3F5",
+      borderRadius: 17,
+      paddingHorizontal: 20,
+      paddingVertical: 8,
+      marginHorizontal: 5,
+      width: 100,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    tagButtonChoice: {
+      width: 100,
+      backgroundColor: "#FDE1EB",
+      borderRadius: 17,
+      paddingHorizontal: 20,
+      paddingVertical: 8,
+      marginHorizontal: 5,
+    },
+    tagChoice: {
+      color: "#C84771",
+    },
+    tagText: {
+      color: "black",
+    },        
+    top: {
+      marginTop: 10,
+    },
+    trend: {
+      fontWeight: "bold",
+      fontSize: 35,
+    },
+    name: {
+      fontWeight: "bold",
+      fontSize: 15,
+    },
+    podcast: {
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    trendingContainer: {
+      marginTop: 20,
+      flexDirection: "row",
+    },
+    trendingColumn: {
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      flex: 1,
+    },
+    
   });
   
--- app/screens/Purchase/history.js
+++ app/screens/Purchase/history.js
@@ -1,110 +1,140 @@
 import {
-    SafeAreaView,
-    View,
-    Text,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity,
-    StatusBar,
-  } from "react-native";
-  import { Tab, TabView } from "@rneui/themed";
-  import React, { useState, useEffect } from "react";
-  import MapView from "react-native-maps";
-  import Ionicons from "react-native-vector-icons/Ionicons";
-  
-  export default function PodCastScreen({ navigation }) {
-    const [text, onChangeText] = React.useState("Useless Text");
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.container1}>
-            <Tab>
-                <Tab.Item title="History" titleStyle={styles.tabTitle} />
-                <Tab.Item title="Order" titleStyle={styles.tabTitle} />
-                <Tab.Item title="Checkout" titleStyle={styles.tabTitle} />
-            </Tab>
-        </View>
-  
-      </SafeAreaView>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "white",
-    },
-    container1: {
-        paddingTop: 30,
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: "center",
-    },
-    lookup_text: {
-      alignItems: "center",
-    },
-  
-    lookup_text_header: {
-      fontSize: 20,
-      fontWeight: 500,
-    },
-  
-    lookup_text_body: {
-      fontSize: 14,
-      color: "#C7C5CD",
-    },
-    lookup: {
-      position: "absolute",
-      alignItems: "center",
-      bottom: 16,
-      left: 16,
-      right: 16,
-      width: "45%",
-      height: 122,
-      backgroundColor: "white",
-      borderRadius: 20,
-    },
-  
-    searchSection: {
-      position: "absolute",
-      top: 16 + StatusBar.currentHeight,
-      left: 16,
-      right: 16,
-      flex: 1,
-    },
-  
-    searchSection1: {
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      borderWidth: 1,
-      borderRadius: 8,
-      height: 58,
-      width: "100%",
-      borderColor: "#F5C0D2",
-      backgroundColor: "white",
-    },
-    searchIcon: {
-      padding: 10,
-    },
-  
-    searchIcon_lookup: {
-      paddingBottom: 5,
-      paddingTop: 13,
-    },
-  
-    input: {
-      flex: 1,
-    },
-    welcome: {
-        fontWeight: "bold",
-        fontSize: 18,
-        width: '100%',
-        textAlign: "center",
-        paddingBottom: 15,
-      },
-      tabTitle: {
-        color: "black", 
-        fontSize: 15,
-      }
-  });
-  
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { Tab, TabView } from "@rneui/themed";
+import React, { useEffect, useState } from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import axios from "axios";
+
+export default function History({ navigation }) {
+  const [index, setIndex] = React.useState(0);
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container1}>
+        <Tab value={index} onChange={(e) => setIndex(e)}>
+            <Tab.Item title="History" titleStyle={styles.tabTitle} />
+            <Tab.Item title="Order" titleStyle={styles.tabTitle} />
+            <Tab.Item title="Checkout" titleStyle={styles.tabTitle} />
+        </Tab>
+
+        <TabView
+          value={index}
+          onChange={setIndex}
+          animationType="spring"
+          style={{ backgroundColor: "white" }}
+        >
+          <TabView.Item>
+            <Text style={styles.font}>1</Text>
+          </TabView.Item>
+          <TabView.Item title="Order">
+            <Text>2</Text>
+          </TabView.Item>
+          <TabView.Item>
+            <Text>3</Text>
+          </TabView.Item>
+        </TabView>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  route_item: {
+    backgroundColor: "white",
+    margin: 16,
+    marginTop: 0,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  route_item_foot: {
+    flexDirection: "row",
+    backgroundColor: "#F5C0D2",
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+
+  route_item_foot_text: {
+    flexDirection: "row",
+    color: "black",
+    fontSize: 16,
+  },
+
+  route_item_head: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "60%",
+  },
+
+  route_item_head_title: {
+    fontSize: 24,
+    fontWeight: 500,
+    color: "#BC305D",
+  },
+
+  route_item_head_text: {
+    width: "100%",
+  },
+
+  route_item_head_body: {
+    fontSize: 14,
+  },
+
+  busicon: {
+    padding: 32,
+  },
+
+  route_item_foot_part: {
+    width: "50%",
+    padding: 12,
+    alignItems: "center",
+  },
+
+  container: {
+    flex: 1,
+  },
+
+  searchSection: {
+    flex: 1,
+    padding: 16,
+  },
+
+  searchSection1: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 8,
+    height: 58,
+    width: "100%",
+    borderColor: "#F5C0D2",
+    backgroundColor: "white",
+  },
+  font: {
+    paddingTop:100,
+  },
+
+  input: {
+    flex: 1,
+  },
+  container1: {
+    paddingTop: 30,
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: "center",
+  },
+  tabTitle: {
+    color: "black", 
+    fontSize: 15,
+  },
+});
