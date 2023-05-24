cd59f98c97fe2fcb637fcdf999fd534a5122a9ed
Nguyễn Mậu Đức <nguyenmauduc91@gmail.com>
2023-05-24 14:43:03 +0700

    Revert "Fixed Profile"
    This reverts commit c2f0aef7e68d8534426e46716385d718796a0ea3.

--- app/screens/Login/Signin.js
+++ app/screens/Login/Signin.js
@@ -2,120 +2,95 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { ImageBackground } from "react-native";
-import {
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  TouchableOpacity,
-  TextInput,
-  Alert,
-  Linking,
-} from "react-native";
+import { Image, StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Alert, Linking } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Login = ({ navigation }) => {
   const handleLogin = () => {
-    return navigation.navigate("Main");
+    return navigation.navigate('Main');
   };
 
   const create = () => {
-    return navigation.navigate("Signup");
+    return navigation.navigate('Signup');
   };
 
   const handleForgot = () => {
-    return navigation.navigate("Forgot");
+    return navigation.navigate('Forgot')
   };
 
   return (
     <>
-      <View style={styles.container}>
-        <View style={styles.container1}>
-          <TouchableOpacity style={styles.thanh} onPress={handleLogin}>
-            <View style={styles.row}>
-              <Text style={styles.thanhText}>
-                Welcome to BusPlus as a guest
-              </Text>
-              <Ionicons
-                style={styles.padd}
-                name={"chevron-forward-outline"}
-                size={20}
-                color={"#FFFFFF"}
-              ></Ionicons>
-            </View>
-          </TouchableOpacity>
-        </View>
+    <View style = {styles.container}>
 
-        <View style={styles.container2}>
-          <Image
-            style={styles.logo}
-            source={require("../../assets/test.png")}
-          />
-        </View>
+      <View style = {styles.container1}>
+        <TouchableOpacity style={styles.thanh} onPress={handleLogin}>
+          <View style={styles.row}>
+            <Text style={styles.thanhText}>Welcome to BusPlus as a guest</Text>
+            <Ionicons style={styles.padd} name={"chevron-forward-outline"} size={20} color={"#FFFFFF"}></Ionicons>
+          </View>
+        </TouchableOpacity>
+      </View>
 
-        <View style={styles.container2}>
-          <View style={styles.form}>
-            <View style={styles.formcenter}>
-              <TextInput
-                style={styles.input}
-                placeholder="Username"
-                // onChangeText={(text) => setUsername(text)}
-              />
-            </View>
+      <View style={styles.container2}>          
+        <Image style={styles.logo} source={require("../../assets/test.png")} />
+      </View>
+
+      <View style={styles.container2}>        
+        <View style={styles.form}>
+          <View style={styles.formcenter}>
+            <TextInput
+              style={styles.input}
+              placeholder="Username"
+              // onChangeText={(text) => setUsername(text)}
+            />
           </View>
         </View>
+      </View>
 
-        <View style={styles.container2}>
-          <View style={styles.form}>
-            <View style={styles.formcenter}>
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry={true}
-                // onChangeText={(text) => setPassword(text)}
-              />
-            </View>
-            <View style={styles.captionContainer}>
-              <TouchableOpacity onPress={handleForgot}>
-                <View>
-                  <Text style={styles.caption}>Forgot Password</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
+      <View style={styles.container2}>     
+        <View style={styles.form}>
+          <View style={styles.formcenter}>
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry={true}
+              // onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+          <View style={styles.captionContainer}>
+            <TouchableOpacity onPress={handleForgot}>
+              <View>
+                <Text style={styles.caption}>Forgot Password</Text>
+              </View>
+            </TouchableOpacity>    
           </View>
         </View>
+      </View>
 
-        <View style={styles.container3}></View>
+      <View style = {styles.container3}></View>
 
-        <View style={styles.content}>
-          <TouchableOpacity style={styles.button} onPress={handleLogin}>
-            <Text style={styles.thanhText}>Welcome to BusPlus</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.thanhText}>Welcome to BusPlus</Text>
+        </TouchableOpacity>
+      </View>    
 
-        <View style={styles.container3}>
-          <Image style={styles.logo1} source={require("../../assets/fb.png")} />
-          <Image
-            style={styles.logocenter}
-            source={require("../../assets/mail.png")}
-          />
-          <Image style={styles.logo1} source={require("../../assets/dt.png")} />
-        </View>
+      <View style = {styles.container3}>
+        <Image style={styles.logo1} source={require("../../assets/fb.png")} />
+        <Image style={styles.logocenter} source={require("../../assets/mail.png")} />
+        <Image style={styles.logo1} source={require("../../assets/dt.png")} />
+      </View>
 
-        <View style={styles.container3}>
-          <TouchableOpacity style={styles.thanh} onPress={create}>
-            <View style={styles.row}>
-              <Text style={styles.thanhText}>Create Account</Text>
-              <Ionicons
-                style={styles.padd}
-                name={"chevron-forward-outline"}
-                size={20}
-                color={"#FFFFFF"}
-              ></Ionicons>
-            </View>
-          </TouchableOpacity>
-        </View>
+
+      <View style = {styles.container3}>
+        <TouchableOpacity style={styles.thanh} onPress={create}>
+          <View style={styles.row}>
+            <Text style={styles.thanhText}>Create Account</Text>
+            <Ionicons style={styles.padd} name={"chevron-forward-outline"} size={20} color={"#FFFFFF"}></Ionicons>
+          </View>
+        </TouchableOpacity>
+      </View>
+
       </View>
     </>
   );
@@ -126,7 +101,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
     justifyContent: "space-between",
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   container1: {
     flexDirection: "row",
@@ -135,12 +110,12 @@ const styles = StyleSheet.create({
   },
   container3: {
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: 'center',
     justifyContent: "center",
   },
   row: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   thanh: {
     width: "100%",
@@ -159,13 +134,13 @@ const styles = StyleSheet.create({
   },
   container2: {
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent:"center",
   },
 
   container4: {
-    width: "90%",
+    width: '90%',
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent:"center",
   },
 
   logo: {
@@ -187,6 +162,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center", // can giua theo truc y
     alignItems: "center", // can giua theo truc x
+
   },
 
   form: {
@@ -201,17 +177,17 @@ const styles = StyleSheet.create({
   form1: {
     width: "100%",
   },
-
+  
   input: {
     width: "90%",
     height: 50,
     backgroundColor: "white",
     borderRadius: 10,
-    borderColor: "#C7C7CC",
+    borderColor: '#C7C7CC',
     borderWidth: 1,
     padding: 10,
   },
-
+  
   button: {
     width: "90%",
     height: 45,
@@ -222,11 +198,12 @@ const styles = StyleSheet.create({
   },
 
   caption: {
-    color: "black",
+    color: 'black',
     fontWeight: "bold",
-    textAlign: "right",
+    textAlign: 'right',
     marginRight: 19,
   },
+
 });
 
 export default Login;
